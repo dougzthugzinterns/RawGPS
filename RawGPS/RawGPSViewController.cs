b0a5efc4965372ff3a47d4a31c661ae978efc2a3
dougzthugzinterns <freerjm@egb-10-33-10-193.muo.muohio.edu>
2013-06-26 21:04:35 -0400

    Final Commit 1
    Final Versions of:
    getCurrentLatitude()
    getCurrentLongitude()
    getCurrentSpeed()

--- RawGPS/RawGPSViewController.cs
+++ RawGPS/RawGPSViewController.cs
@@ -15,6 +15,15 @@ namespace RawGPS
 		{
 		}
 
+		partial void recLoc (NSObject sender)
+		{
+			//gets current latitude of the user and changes the "Recorded Latitude Label" to include the current latitude.
+			double lat = this.getCurrentLatitude();
+			RecLatLabel.Text = lat.ToString();
+			//gets current longitude of the user and changes the "Recorded Longitude Label" to include the current longitude.
+			double lon = this.getCurrentLongitude();
+			RecLongLabel.Text = lon.ToString();
+		}
 		public override void DidReceiveMemoryWarning ()
 		{
 			// Releases the view if it doesn't have a superview.
@@ -22,24 +31,54 @@ namespace RawGPS
 			
 			// Release any cached data, images, etc that aren't in use.
 		}
-
+		//just a sample application that updates labels when the location is updated.
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-
 			myLocManager = new CLLocationManager ();
-
 			myLocManager.DesiredAccuracy = 10;
-
 			myLocManager.LocationsUpdated += (object sender, CLLocationsUpdatedEventArgs e) => {
 				LatLabel.Text = e.Locations [e.Locations.Length - 1].Coordinate.Latitude.ToString() + "degrees";
 				LongLabel.Text = e.Locations [e.Locations.Length - 1].Coordinate.Longitude.ToString() + "degrees";
 				SpeedLabel.Text = e.Locations [e.Locations.Length - 1].Speed.ToString() + "meters/sec";
 			};
-			
-			// Perform any additional setup after loading the view, typically from a nib.
+			if(CLLocationManager.LocationServicesEnabled){
+				myLocManager.StartUpdatingLocation ();
+			}
 		}
-
+		//Gets the Latitude of the user.
+		public double getCurrentLatitude(){
+			CLLocationManager myLocMan = new CLLocationManager ();
+			myLocMan.DesiredAccuracy = 10; //accuracy within ten meters.
+			if(CLLocationManager.LocationServicesEnabled){
+				myLocMan.StartUpdatingLocation (); //starts updating the location.
+			}
+			double latitude = myLocMan.Location.Coordinate.Latitude;
+			myLocMan.StopUpdatingLocation (); //stops updating the location.
+			return latitude;
+		}
+		//Gets the Longitude of the user.
+		public double getCurrentLongitude(){
+			CLLocationManager myLocMan = new CLLocationManager ();
+			myLocMan.DesiredAccuracy = 10; //accuracy within ten meters.
+			if(CLLocationManager.LocationServicesEnabled){
+				myLocMan.StartUpdatingLocation (); //starts updating the location.
+			}
+			myLocMan.StopUpdatingLocation (); //stops updating the location.
+			double longitude = myLocMan.Location.Coordinate.Longitude;
+			return longitude;
+		}
+		//Gets the Speed of the user.
+		public double getCurrentSpeed(){
+			CLLocationManager myLocMan = new CLLocationManager ();
+			myLocMan.DesiredAccuracy = 10; //accuracy within ten meters.
+			if(CLLocationManager.LocationServicesEnabled){
+				myLocMan.StartUpdatingLocation (); //starts updating the location.
+			}
+			double speed = myLocMan.Location.Speed;
+			myLocMan.StopUpdatingLocation ();
+			return speed;
+		}
 		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
 		{
 			// Return true for supported orientations
